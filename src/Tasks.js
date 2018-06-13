ffb52c0dcc4e96206cc7aefb078c70fc6499df63
Noah French <noahjfrench@gmail.com>
2018-06-13 11:46:16 -0400

    Made the skeleton of the app with a navigation bar and routing to blank pages!

---
+++ src/Tasks.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import TopbarUser from "./TopbarUser.js";
+
+export default class Tasks extends Component {
+  render() {
+    return (
+      <div>
+        <TopbarUser />Tasks Page
+      </div>
+    );
+  }
+}
---
+++ src/TopbarCompany.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "antd";
+import "./Topbar.css";
+import "antd/dist/antd.css";
+
+const MenuItemGroup = Menu.ItemGroup;
+
+export default class TopbarCompany extends Component {
+  render() {
+    return (
+      <div>
+        <Menu mode="horizontal">
+          <Menu.Item key="about">
+            <Link to="/about">about</Link>
+          </Menu.Item>
+          <Menu.Item key="meet-the-team">
+            <Link to="/meet-the-team">meet the team</Link>
+          </Menu.Item>
+          <Menu.Item key="submit-contracts">
+            <Link to="/submit-contracts">submit contracts</Link>
+          </Menu.Item>
+          <Menu.Item key="login">
+            <Link to="/login">members login</Link>
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
+  }
+}
