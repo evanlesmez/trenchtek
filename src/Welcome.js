b2755fcf4b0bf1c2df3790461b964945bef36524
Sonali Luthar <sonaliluthar@d-172-25-79-46.dhcp.virginia.edu>
2018-06-13 16:12:51 -0400

    task page in progress

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import TopbarCompany from "./TopbarCompany.js";
+import "./Welcome.css";
+
+//const imageUrl = require("./deskimage.jpg");
 
 export default class Welcome extends Component {
   render() {
-    console.log("this is the welcome page!!");
     return (
-      <div>
-        <TopbarCompany />
-        Welcome Page
+      // <img src="./deskimage.jpg" alt="bg" class="bg" />
+      <div class="welcbg">
+        <div>
+          <TopbarCompany />
+          Welcome Page
+        </div>
       </div>
     );
   }
 }
+
+{
+  /* <div id="bg">
+          <img src="./deskimage.jpg" alt="" />
+        </div> */
+}
+
+// style={{ backgroundImage: "url($" + { imageUrl } + " )" }}
