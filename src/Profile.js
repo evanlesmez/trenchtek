f3b244388788f29fcf2e8a4c08d9b92316e4639b
evanlesmez <evan.lesmez@gmail.com>
2018-06-13 17:36:58 -0400

    Profile page work

--- src/Profile.js
+++ src/Profile.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import TopbarUser from "./TopbarUser.js";
-
+import "./Profile.css";
 
 
 export default class Profile extends Component {
   render() {
     return (
-      <div>
+      <div >
         <TopbarUser />Profile Page
+        <div className="center">
+        <div>
+        <img className="profpic" src= "http://static.tvtropes.org/pmwiki/pub/images/reg_anime.jpg" alt = "Prof pic"/>
+        </div>
         <button type= "edit"> Edit </button>
+        </div>
       </div>
     );
   }
