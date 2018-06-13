1f58ef4efed9c077da2cf52de64dfc4ff1db1e52
evanlesmez <evan.lesmez@gmail.com>
2018-06-13 17:11:52 -0400

    Working on profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import TopbarUser from "./TopbarUser.js";
 
+
+
 export default class Profile extends Component {
   render() {
     return (
       <div>
         <TopbarUser />Profile Page
+        <button type= "edit"> Edit </button>
       </div>
     );
   }
