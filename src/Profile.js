977106f346fbdc8e5ab4aacc4dfff00c94f1a260
evanlesmez <evan.lesmez@gmail.com>
2018-06-14 14:28:41 -0400

    Profile css is alright for now, trying to get functionality going

--- src/Profile.js
+++ src/Profile.js
@@ -1,18 +1,55 @@
 import React, { Component } from "react";
 import TopbarUser from "./TopbarUser.js";
 import "./Profile.css";
+import { Row, Col } from 'antd';
 
-
+// Banner from https://www.google.com/search?q=codding+banner&rlz=1C1CHBF_enUS765US765&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjm7KW7sNPbAhVJ3VMKHWUZBioQ_AUICigB&biw=1536&bih=734&dpr=1.25#imgrc=vAFXqrj7GeFLsM:}
 export default class Profile extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      editing: false,
+      name: props.name,
+      aboutMe: props.AboutMe,
+      jobTitle: props.jobTitle,
+      skills: props.skills
+    }
+  }
   render() {
     return (
       <div >
-        <TopbarUser />Profile Page
+        <TopbarUser />
         <div className="center">
-        <div>
-        <img className="profpic" src= "http://static.tvtropes.org/pmwiki/pub/images/reg_anime.jpg" alt = "Prof pic"/>
-        </div>
-        <button type= "edit"> Edit </button>
+          <div id="imgcontainer">
+          <img className = "banner" src = "http://www.twitrcovers.com/wp-content/uploads/2013/02/Programming-Code-l.jpg" alt = "banner"/>
+          <Row type= "flex" gutter= {15}>
+            <Col span= {4}>
+          <img className="profpic" src= "http://static.tvtropes.org/pmwiki/pub/images/reg_anime.jpg" alt = "Prof pic"/> 
+            </Col>
+            <Col span ={20}>
+            <div id= "userfo" >
+            <h1  maxlength= "20"> {this.props.name} </h1>
+            <h2> Job Title </h2>
+            <h3> About me: </h3>
+            <textarea  maxlength = '50'> {this.props.aboutMe}</textarea>
+            <div>
+              <div>
+               Skills: 
+              </div>
+            
+              <span>
+             <a href=""> Github</a>
+             <a href=""> LinkedIn</a>
+             <a href=""> </a>
+             </span>
+            </div>
+            </div>
+            </Col>
+          </Row>
+          </div>  
+          <div className ="profside">
+          <button type= "edit"> Edit Pic </button>
+          </div>
         </div>
       </div>
     );
--- src/RouteC.js
+++ src/RouteC.js
@@ -5,24 +5,25 @@ import About from "./About.js";
 import Meet from "./Meet.js";
 import SubmitContracts from "./SubmitContracts.js";
 import Login from "./Login.js";
-import Profile from "./Profile.js";
+import Profilehandler from "./Profilehandler.js";
 import Tasks from "./Tasks.js";
 import BrowseContracts from "./BrowseContracts.js";
 import Connect from "./Connect.js";
 import Resources from "./Resources.js";
+
 
 export default class RouteC extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Redirect to="/profile" />
+          <Redirect to="/profilehandler" />
           <Route path="/welcome" component={Welcome} />
           <Route path="/about" component={About} />
           <Route path="/meet-the-team" component={Meet} />
           <Route path="/submit-contracts" component={SubmitContracts} />
           <Route path="/login" component={Login} />
-          <Route path="/profile" component={Profile} />
+          <Route path="/profilehandler" component={Profilehandler} />
           <Route path="/tasks" component={Tasks} />
           <Route path="/browse-contracts" component={BrowseContracts} />
           <Route path="/connect" component={Connect} />
