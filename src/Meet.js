0882b0daa94e6ee8c26daebfe8e79fe8dada6d25
eve9du <eve9du@virginia.edu>
2018-06-14 16:10:18 -0400

    Complete design of Meet the Team, minues edit option

--- src/Meet.js
+++ src/Meet.js
@@ -8,10 +8,14 @@ export default class Meet extends Component {
   state = {
     instructors: [{
       name: "Bruno", tags: "#Javascipt #Python",
-      info: <p>The name Braid signifies the interweaving of individuals, teams, skill sets and experiences, where the resulting whole is stronger than the sum of its&nbsp;parts.</p> <p>We are better together. This not only true of Braid’s internal team, but also of our collaborations with partners and&nbsp;vendors.</p> <p>Our belief is that a rising tide lifts all boats. As a result, we value cooperation over competition, and service above being served.</p>
+      info: "Bruno is a wonderful instructor. He taugh for 5 years in Cambridge, but got kicked out for sleeping with one of his students. Despite his misconducts, Bruno is still considered one of the top 10 instuctors of the world and attends talks at various universities and schools. His interests include software engineering, playing the banjo, trombone, and the pipes. He has a flamboyant personality and is very friendly to both humans and animals. "
     },
-    { name: "eliza", tags: "nothing", info: "The name Braid signifies the interweaving of individuals, teams, skill sets and experiences, where the resulting whole is stronger than the sum of its parts.</p><p>We are better together. This not only true of Braid’s internal team, but also of our collaborations with partners and vendors.</p><p>Our belief is that a rising tide lifts all boats. As a result, we value cooperation over competition, and service above being served." }],
-    seniorDevs: []
+    { name: "Eliza", tags: "#legitknowseverything", info: " Eliza is hands down the best software engineer that has ever lived. Apart from that, she is quite the business woman. In contrast to common knowledge, she was the creator of Google, Facebook, Twitter, Amazon, Apple and Microsoft. Even though she has had her ideas stolen too many times, Eliza remains content with her contributions to the world and now teaches in our company in order to pass down her knowledge and experiences to our own lucky interns" }],
+    seniorDevs: [{
+      name: "Bruno", tags: "#Javascipt #Python",
+      info: "Bruno is a wonderful instructor. He taugh for 5 years in Cambridge, but got kicked out for sleeping with one of his students. Despite his misconducts, Bruno is still considered one of the top 10 instuctors of the world and attends talks at various universities and schools. His interests include software engineering, playing the banjo, trombone, and the pipes. He has a flamboyant personality and is very friendly to both humans and animals. "
+    },
+    { name: "Eliza", tags: "#legitknowseverything", info: " Eliza is hands down the best software engineer that has ever lived.Apart from that, she is quite the business woman. In contrast to common knowledge, she was the creator of Google, Facebook, Twitter, Amazon, Apple and Microsoft. Even though she has had her ideas stolen too many time, Eliza remains content with her contributions to the world and now teaches in our company in order to pass down her knowledge and experiences to our own lucky interns" }]
   }
   render() {
     return (
@@ -39,7 +43,7 @@ export default class Meet extends Component {
 
             {this.state.instructors.map(instructor => {
               return (
-                <div>
+                <div className="individualCards" >
                   <Card className="InstructorCard" style={{ width: 240 }} bodyStyle={{ padding: 0 }}>
                     <div className="custom-image">
                       <img alt="example" width="100%" src="https://www.shrs.pitt.edu/sites/default/files/default_images/default-person.jpg" />
@@ -59,11 +63,34 @@ export default class Meet extends Component {
         </div>
 
         <div>
-
-
           <div className="seniorDevs">
-            <h1 > Our Senior Developers </h1>
+            <h1 > Our Senior Developers</h1>
+
           </div>
+
+
+          <div className="infocard">
+
+            {this.state.instructors.map(instructor => {
+              return (
+                <div className="individualCards" >
+                  <Card className="InstructorCard" style={{ width: 240 }} bodyStyle={{ padding: 0 }}>
+                    <div className="custom-image">
+                      <img alt="example" width="100%" src="https://www.shrs.pitt.edu/sites/default/files/default_images/default-person.jpg" />
+                    </div>
+                    <div className="custom-card">
+                      <div className="textOfCard">
+                        <h3>{instructor.name}</h3>
+                        {/* <p>linkedIn</p> */}
+                      </div>
+                    </div>
+                  </Card>
+                  <div className="texty"> <div><p>{instructor.info}</p> <h5>Tags: {instructor.tags}</h5></div> </div>
+                </div>
+              );
+            })}
+          </div>
+
 
         </div>
       </div>
