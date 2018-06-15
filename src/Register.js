9c7bebbd53c4499811940b22996ffcfe1d89f01b
Noah French <noahjfrench@gmail.com>
2018-06-15 16:09:51 -0400

    make a pretty welcome page

--- src/Register.js
+++ src/Register.js
@@ -104,8 +104,7 @@ class Register extends Component {
     const {
       getFieldDecorator,
       getFieldsError,
-      isFieldTouched,
-      getFieldError
+      isFieldTouched
     } = this.props.form;
     const formItemLayout = {
       labelCol: {
