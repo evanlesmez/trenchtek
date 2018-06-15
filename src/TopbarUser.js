38d0c9d9ca10a9c02fc8d44f393f11b4e8c158f3
Sung Joon Park <andrewpark1136@hotmail.com>
2018-06-15 13:07:39 -0400

    chat8

--- src/TopbarUser.js
+++ src/TopbarUser.js
@@ -20,74 +20,78 @@ const menu = (
 );
 export default class TopbarUser extends Component {
   render() {
-  if(true){
-    return (
-      <div>
-        <Menu mode="horizontal">
-          <Menu.Item key="tasks">
-            <Link to="/tasks">tasks</Link>
-          </Menu.Item>
-          <Menu.Item key="browse-contracts">
-            <Link to="/browse-contracts">contracts</Link>
-          </Menu.Item>
-          <Menu.Item key="connect">
-            <Link to="/connect">connect</Link>
-          </Menu.Item>
-          <Menu.Item key="resources">
-            <Link to="/resources">resources</Link>
-          </Menu.Item>
-          <Menu.Item key="profile">
-            <Link to="/profile">profile</Link>
-          </Menu.Item>
-          <Menu.Item key="admin">
-            <Link to="/admin">admin</Link>
-          </Menu.Item>
-        </Menu>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <Menu mode="horizontal" defaultSelectedKeys="profile">
-          <Menu.Item key="profile">
-            <Link to="/profile">
-              <div className="topbar-tab">profile</div>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="challenges">
-            <Link to="/challenges">
-              <div className="topbar-tab">challenges</div>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="task-manager">
-            <Link to="/task-manager">
-              <div className="topbar-tab">task manager</div>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="browse-contracts">
-            <Link to="/browse-contracts">
-              <div className="topbar-tab">contracts</div>
-            </Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Dropdown key="connect" overlay={menu}>
-              <div className="topbar-tab">connect</div>
-            </Dropdown>
-          </Menu.Item>
-          <Menu.Item key="resources">
-            <Link to="/resources">
-              <div className="topbar-tab">resources</div>
-            </Link>
-          </Menu.Item>
-          <Button className="login-logout-button" type="danger" ghost>
-            <Link to="/logout">
-              <div className="topbar-tab">logout</div>
-            </Link>
-          </Button>
-        </Menu>
-      </div>
-    );
+    if (true) {
+      return (
+        <div>
+          <Menu mode="horizontal">
+            <Menu.Item key="profile">
+              <Link to="/profile">profile</Link>
+            </Menu.Item>
+            <Menu.Item key="tasks">
+              <Link to="/tasks">tasks</Link>
+            </Menu.Item>
+            <Menu.Item key="browse-contracts">
+              <Link to="/browse-contracts">contracts</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Dropdown overlay={menu}>
+                <a className="ant-dropdown-link" href="#">
+                  connect <Icon type="down" />
+                </a>
+              </Dropdown>
+            </Menu.Item>
+            <Menu.Item key="resources">
+              <Link to="/resources">resources</Link>
+            </Menu.Item>
 
-  }
+            <Menu.Item key="admin">
+              <Link to="/admin">admin</Link>
+            </Menu.Item>
+          </Menu>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Menu mode="horizontal" defaultSelectedKeys="profile">
+            <Menu.Item key="profile">
+              <Link to="/profile">
+                <div className="topbar-tab">profile</div>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="challenges">
+              <Link to="/challenges">
+                <div className="topbar-tab">challenges</div>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="task-manager">
+              <Link to="/task-manager">
+                <div className="topbar-tab">task manager</div>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="browse-contracts">
+              <Link to="/browse-contracts">
+                <div className="topbar-tab">contracts</div>
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Dropdown key="connect" overlay={menu}>
+                <div className="topbar-tab">connect</div>
+              </Dropdown>
+            </Menu.Item>
+            <Menu.Item key="resources">
+              <Link to="/resources">
+                <div className="topbar-tab">resources</div>
+              </Link>
+            </Menu.Item>
+            <Button className="login-logout-button" type="danger" ghost>
+              <Link to="/logout">
+                <div className="topbar-tab">logout</div>
+              </Link>
+            </Button>
+          </Menu>
+        </div>
+      );
+    }
   }
 }
