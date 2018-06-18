54138782dd63f25ee34e0e21568491f051a461d3
Noah French <noahjfrench@gmail.com>
2018-06-18 11:31:38 -0400

    add some breaks to properly style resources page

--- src/Resources.js
+++ src/Resources.js
@@ -78,6 +78,7 @@ class Resources extends Component {
         <div>
           <center>
             <br />
+            <br />
             <Card title="Add Resource" style={{ width: 600 }}>
               <Form layout="vertical" className="login-form">
                 <Form.Item>
@@ -154,6 +155,7 @@ class Resources extends Component {
     return (
       <div>
         <center>
+          <br />
           <br />
           <Card title="Resources" style={{ width: "85%" }}>
             {this.state.resources.map(resource => {
