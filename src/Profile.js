63890aa77ba62fc9dc59080847647c62127227f2
evanlesmez <evan.lesmez@gmail.com>
2018-06-18 16:13:01 -0400

    Profile is set for adding picture and reading, now just need to do firebase database

--- src/Profile.js
+++ src/Profile.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import TopbarUser from "./TopbarUser.js";
 import "./Profile.css";
 import { Row, Col, Icon } from 'antd';
-import firebase from "./Firebase";
+import firebase from "./Firebase.js";
 // import Modal from 'react-modal'; 
-
+//Reg: "http://static.tvtropes.org/pmwiki/pub/images/reg_anime.jpg"
 // Banner from https://www.google.com/search?q=codding+banner&rlz=1C1CHBF_enUS765US765&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjm7KW7sNPbAhVJ3VMKHWUZBioQ_AUICigB&biw=1536&bih=734&dpr=1.25#imgrc=vAFXqrj7GeFLsM:}
+
+let storageRef = firebase.storage().ref('images');
+let profRead = firebase.storage().ref('images/');
+let userRef = firebase.database().ref();
 export default class Profile extends Component {
   constructor(props){
     super(props)
@@ -21,7 +25,10 @@ export default class Profile extends Component {
       inputclass: "inputfield",
       email: props.userData.links.email,
       github: props.userData.links.github,
-      LinkedIn: props.userData.links.LinkedIn
+      LinkedIn: props.userData.links.LinkedIn,
+      banner: "",
+      profFile: "",
+      profURL: props.userData.profURL
     }
   }
   handleChange = (e) => { // handles changes in text entries
@@ -58,20 +65,42 @@ export default class Profile extends Component {
       links: copySkills
     })
   }
-
+  firePost = (e) => {
+    let reader = new FileReader();  // API for proccessing files
+    let file = e.target.files[0];
+    reader.onloadend = () => {
+      storageRef.child(file.name).put(file)
+      this.setState({profFile:file, profURL: reader.result});
+    }
+    reader.onerror = e => {
+      console.log("Failed file read: " + e.toString());
+    }
+    reader.readAsDataURL(file)
+  }
+  componentDidMount(){  // Updates the picture
+    // Get the download URL
+      profRead.child('DanielSmiley.jpg')
+        .getDownloadURL().then( url => {
+          console.log(url);
+          this.setState({profURL:url});
+      }).catch(function(error) {
+      });
+    }
   render() {
     let button; // Conditional rendering
     let skillSpan;
     let newSkill;
     let linksPart;
     let profypic;
-    if (this.state.editing == false) {
+    if (this.state.editing == false) {    // FOR NOMRAL PAGE
       button = <button type= "edit" onClick={e=>this.editPress(e)}>Edit</button> //Editbutton
+      
       skillSpan = this.state.skills.map((skill) => {  //FOr displaying skills (also removable)
         return(
          <span> #{skill} </span>
         )
       });
+      
       linksPart = <div>
               <p>
                 Contact: <a href= {this.state.email}> {this.state.email}</a>
@@ -81,9 +110,12 @@ export default class Profile extends Component {
              <a href={this.state.LinkedIn}> LinkedIn</a>
              </span>
              </div>
-    
-      } else {  // All of the input fields
+      
+      profypic = <img className="profpic" src= {this.state.profURL} alt = "Prof pic"/> 
+      
+    } else {  // All of the input fields
       button = <button type= "save" onClick={e => this.saveClick(e)}>Save</button> 
+      
       skillSpan = this.state.skills.map((skill) => {
         return(
          <span> 
@@ -91,13 +123,14 @@ export default class Profile extends Component {
            <Icon id="smallicon" type="close-circle" name = {skill} onClick ={e=>this.deleteClick(e)}/>
          </span>
         )});
-      newSkill = 
-      <div>
+
+      newSkill = <div>
       <input name= "newSkill" type = "text" pattern ="[A-Za-z]{0-9}{15}" 
         placeholder="#New skill" size= "10"
          onChange= {e => this.handleChange(e)} value ={this.state.newSkill} ></input>
          <Icon id="smallicon" type="plus-circle" onClick = {e=> this.addClick(e)}/>
       </div>
+
       linksPart = <div>
       <p>
         Contact: <input type= "url" placeholder = "your@email.com" name= "email" 
@@ -110,7 +143,12 @@ export default class Profile extends Component {
      value={this.state.LinkedIn} onChange= {e=> this.handleChange(e)}/>
      </span>
      </div>
-     profypic = <input type = "file" name = "profpic" accept = "image/"> </input>
+
+     profypic =  <div>
+     <img className = "profpic" src ={this.state.profURL} alt = 'profpic'/>
+     <input type = "file" name = "profpic" accept= "image/" onChange = {e => this.firePost(e)}
+     style = {{'marginLeft':"30px", "paddingTop":"5px"}}/> 
+     </div>
     }
     
     return (
@@ -118,20 +156,20 @@ export default class Profile extends Component {
         <div className="center">
           <div id="imgcontainer">
           <img className = "banner" src = "http://www.twitrcovers.com/wp-content/uploads/2013/02/Programming-Code-l.jpg" alt = "banner"/>
-          <Row type= "flex" gutter= {15}>
-            <Col span= {4}>
-            <img className="profpic" src= "http://static.tvtropes.org/pmwiki/pub/images/reg_anime.jpg" alt = "Prof pic"/> 
+          <Row type= "flex" gutter={4}>
+            <Col span= {6}>
+            {profypic}
             <div className ="underpic">
-            <input id = "inputfield" type="text" name = "title" value={this.state.title} size= "10" /*Size is useful*/ />
+            <input id = "inputfield" type="text" name = "title" value={this.state.title} size= "10" readOnly/*Size is useful*/ />
             </div>
             </Col>
 
-            <Col span ={14}>
+            <Col span ={12}>
             <div id= "userfo" >
-            <input id = {this.state.inputclass} type="text" className="header" name = "name" 
+            <input id = {this.state.inputclass} type="text" className="header" name = "name" // USERNAME!!! 
             value={this.state.name} onChange= {e => this.handleChange(e)} readOnly={this.state.readmode} maxLength='35'/> 
 
-            <input id = {this.state.inputclass} type="text" name = "position" style={{'text-align':"center"}} //Inline styling
+            <input id = {this.state.inputclass} type="text" name = "position" style={{'textAlign':"center"}} //Inline styling
             value={this.state.position} onChange= {e => this.handleChange(e)} readOnly={this.state.readmode} maxLength='35'/>
 
             <h3> About me! </h3>
--- src/Profilehandler.js
+++ src/Profilehandler.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 import Profile from "./Profile.js";
+import firebase from "./Firebase.js";
+// Create a reference to the file we want to download
+let storageRef = firebase.storage().ref('images/');
+let userRef = firebase.database();
 
 // Banner from https://www.google.com/search?q=codding+banner&rlz=1C1CHBF_enUS765US765&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjm7KW7sNPbAhVJ3VMKHWUZBioQ_AUICigB&biw=1536&bih=734&dpr=1.25#imgrc=vAFXqrj7GeFLsM:}
+
 const userData= {name:"Reg", position:"Big Ballin Shotcaller", aboutMe:"dinosaur", skills:["git", "react", "JSON's"],
-            links:{github: "github", email: "email", LinkedIn: "linkedin"}, title: "intern"};
-            
+            links:{github: "github", email: "email", LinkedIn: "linkedin"}, title: "intern", 
+            profURL: "http://www.aminariana.com/assets/placeholders/avatar-39c4f0720c0b9f829e3dc8b644228be492ea900026f4057974840d54b149bb5d.png"};
+
+
 export default class Profilehandler extends Component {
+  state = {userData:{name:"Reg", position:"Big Ballin Shotcaller", aboutMe:"dinosaur", 
+    skills:["git", "react", "JSON's"],
+    links:{github: "github", email: "email", LinkedIn: "linkedin"}, title: "intern", 
+    profURL:  "http://www.aminariana.com/assets/placeholders/avatar-39c4f0720c0b9f829e3dc8b644228be492ea900026f4057974840d54b149bb5d.png"},
+    }
+  componentDidMount(){
+    // Get the download URL
+      storageRef.child('DanielSmiley.jpg')
+        .getDownloadURL().then( url => {
+          let changeURL = this.state.userData;
+          changeURL.profURL = url;
+          // this.setState({userData:changeURL});
+          console.log(this.state.userData);
+        // Insert url into an <img> tag to "download"
+      }).catch(function(error) {
+      });
+  }
   render() {
       return (
         <div >
-            <Profile userData = {userData}/>
+            <Profile userData = {this.state.userData}/>
         </div>
         )
   }
