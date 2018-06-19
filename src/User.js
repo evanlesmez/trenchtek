d3513ad5ae636b524df332a8f03a73cde2fa7f67
Marina Kun <kunmarina@gmail.com>
2018-06-19 16:48:36 -0400

    messing with user page for admin

--- src/User.js
+++ src/User.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import firebase from "./Firebase";
-import { Collapse, Button, Form, Input, DatePicker, Icon, Card } from "antd";
+import {
+  Collapse,
+  Button,
+  Form,
+  Input,
+  DatePicker,
+  Icon,
+  Card,
+  Select
+} from "antd";
 
 // Allows the administrators to view all the registered users
 
@@ -8,7 +17,11 @@ export default class User extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      unapprovedUsers: [],
+      approvedUsers: [],
+      allUsers: [],
+      removedUsers: [],
+      display: "unapprovedUsers"
     };
   }
   componentDidMount() {
@@ -17,21 +30,42 @@ export default class User extends Component {
   pullingUsers = () => {
     const userRef = firebase.database().ref("/users");
     userRef.on("value", snapshot => {
-      let userstemp = [];
+      let unapprovedUserstemp = [];
+      let allUserstemp = [];
+      let approvedUserstemp = [];
+      let removedUserstemp = [];
       snapshot.forEach(value => {
         console.log(value.key);
+        let obj = {
+          name: value.val().name,
+          email: value.val().email,
+          title: value.val().title,
+          approved: value.val().approved,
+          upvotes: value.val().upvotes,
+          tags: value.val().tags,
+          key: value.key
+        };
+        allUserstemp.push(obj);
+        console.log(value.val());
         if (value.val().approved === false && value.val().title !== "removed") {
-          let obj = {
-            name: value.val().name,
-            email: value.val().email,
-            title: value.val().title,
-            key: value.key
-          };
-          userstemp.push(obj);
+          unapprovedUserstemp.push(obj);
+          console.log("?");
+        }
+        if (value.val().approved === true && value.val().title !== "removed") {
+          approvedUserstemp.push(obj);
+        }
+        if (
+          value.val().approved === "removed" &&
+          value.val().title === "removed"
+        ) {
+          removedUserstemp.push(obj);
         }
       });
       this.setState({
-        users: userstemp
+        unapprovedUsers: unapprovedUserstemp,
+        allUsers: allUserstemp,
+        approvedUsers: approvedUserstemp,
+        removedUsers: removedUserstemp
       });
     });
   };
@@ -58,22 +92,143 @@ export default class User extends Component {
       userToDelete.child("approved").set("removed");
     }
   };
+
+  handleDisplay = value => {
+    this.setState({
+      display: value
+    });
+  };
+
+  editUnapproved = user => {};
   render() {
+    console.log(this.state);
     return (
       <div>
-        {this.state.users.map(user => {
-          return (
-            <center>
-              <Card title={user.name} style={{ width: 450 }}>
-                <p>Email: {user.email} </p>
-                <p>Title: {user.title} </p>
-                <Button onClick={() => this.acceptUser(user)}>Accept</Button>
-                <Button onClick={e => this.deleteUser(e, user)}>Reject</Button>
-              </Card>
-              <br />
-            </center>
-          );
-        })}
+        <br />
+        <center>
+          <div className="selectusers">
+            Display:{" "}
+            <Select
+              defaultValue="unapprovedUsers"
+              style={{ width: 170 }}
+              onChange={value => {
+                this.handleDisplay(value);
+              }}
+            >
+              <Select.Option value="unapprovedUsers">
+                Unapproved Users
+              </Select.Option>
+              <Select.Option value="approvedUsers">
+                Approved Users
+              </Select.Option>
+              <Select.Option value="removedUsers">Removed Users</Select.Option>
+              <Select.Option value="allUsers">All Users</Select.Option>
+            </Select>
+          </div>
+        </center>
+        <br />
+        {this.state.display === "unapprovedUsers" &&
+          this.state.unapprovedUsers.map(user => {
+            return (
+              <center>
+                <Card
+                  title={
+                    <div className="panelheader">
+                      <center>
+                        <div className="headertitle">{user.name}</div>
+                      </center>
+                      <div className="chaldelete">
+                        <Button size="small">
+                          <Icon type="edit" />
+                        </Button>
+                      </div>
+                    </div>
+                  }
+                  style={{ width: 450 }}
+                >
+                  <p>Email: {user.email} </p>
+                  <p>Title: {user.title} </p>
+                  <Button onClick={() => this.acceptUser(user)}>Accept</Button>
+                  <Button onClick={e => this.deleteUser(e, user)}>
+                    Reject
+                  </Button>
+                </Card>
+                <br />
+              </center>
+            );
+          })}
+        {this.state.display === "removedUsers" &&
+          this.state.removedUsers.map(user => {
+            return (
+              <center>
+                <Card title={user.name} style={{ width: 450 }}>
+                  <p>Email: {user.email} </p>
+                  <p>Title: {user.title} </p>
+                  <Button onClick={() => this.acceptUser(user)}>Accept</Button>
+                  <Button onClick={e => this.deleteUser(e, user)}>
+                    Reject
+                  </Button>
+                </Card>
+                <br />
+              </center>
+            );
+          })}
+        {this.state.display === "approvedUsers" &&
+          this.state.approvedUsers.map(user => {
+            return (
+              <center>
+                <Card
+                  title={
+                    user.name +
+                    " - " +
+                    user.title.charAt(0).toUpperCase() +
+                    user.title.slice(1)
+                  }
+                  style={{ width: 450 }}
+                >
+                  <p>Email: {user.email} </p>
+                  <p>Title: {user.title} </p>
+                  <p>Approved: {user.approved.toString()}</p>
+                  <p>About: {user.about} </p>
+                  <p>Tags: {user.tags}</p>
+
+                  <Button onClick={() => this.acceptUser(user)}>Accept</Button>
+                  <Button onClick={e => this.deleteUser(e, user)}>
+                    Reject
+                  </Button>
+                </Card>
+                <br />
+              </center>
+            );
+          })}
+        {this.state.display === "allUsers" &&
+          this.state.allUsers.map(user => {
+            return (
+              <center>
+                <Card
+                  title={
+                    user.name +
+                    " - " +
+                    user.title.charAt(0).toUpperCase() +
+                    user.title.slice(1)
+                  }
+                  style={{ width: 450 }}
+                >
+                  <p>Email: {user.email} </p>
+                  <p>Title: {user.title} </p>
+                  <p>Approved: {user.approved.toString()}</p>
+                  <p>About: {user.about} </p>
+                  <p>Tags: {user.tags}</p>
+
+                  <Button onClick={() => this.acceptUser(user)}>Accept</Button>
+                  <Button onClick={e => this.deleteUser(e, user)}>
+                    Reject
+                  </Button>
+                </Card>
+                <br />
+              </center>
+            );
+          })}
       </div>
     );
   }
