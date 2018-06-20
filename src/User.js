026b1ce964e3f1c2f29e2c77e48c907fa0281adf
Marina Kun <kunmarina@gmail.com>
2018-06-20 13:20:38 -0400

    users for admin completed

--- src/User.js
+++ src/User.js
@@ -8,7 +8,9 @@ import {
   DatePicker,
   Icon,
   Card,
-  Select
+  Select,
+  Radio,
+  InputNumber
 } from "antd";
 
 // Allows the administrators to view all the registered users
@@ -21,7 +23,18 @@ export default class User extends Component {
       approvedUsers: [],
       allUsers: [],
       removedUsers: [],
-      display: "unapprovedUsers"
+      display: "unapprovedUsers",
+      edit: false,
+      editUser: "",
+      editName: "",
+      editEmail: "",
+      editTitle: "",
+      editApproved: "",
+      editUpvotes: "",
+      editTags: "",
+      editKey: "",
+      editAbout: "",
+      editImage: ""
     };
   }
   componentDidMount() {
@@ -43,7 +56,9 @@ export default class User extends Component {
           approved: value.val().approved,
           upvotes: value.val().upvotes,
           tags: value.val().tags,
-          key: value.key
+          key: value.key,
+          about: value.val().about,
+          image: value.val().image
         };
         allUserstemp.push(obj);
         console.log(value.val());
@@ -99,9 +114,159 @@ export default class User extends Component {
     });
   };
 
-  editUnapproved = user => {};
+  editUnapproved = (e, user) => {
+    e.preventDefault();
+    console.log("addEdit");
+    this.setState({
+      ...this.state,
+      edit: true,
+      editAbout: user.about,
+      editUser: user,
+      editName: user.name,
+      editEmail: user.email,
+      editTitle: user.title,
+      editApproved: user.approved,
+      editUpvotes: user.upvotes,
+      editTags: user.tags,
+      editKey: user.key,
+      editImage: user.image
+    });
+  };
+
+  updateEditUser = (field, value) => {
+    console.log("update state");
+    console.log(value);
+    this.setState({
+      [field]: value
+    });
+  };
+
+  cancel = () => {
+    this.setState({
+      edit: false
+    });
+  };
+
+  submitChanges = () => {
+    console.log(this.state.editUser);
+    let edit = {
+      about: this.state.editAbout,
+      approved: this.state.editApproved,
+      email: this.state.editEmail,
+      image: this.state.editImage,
+      name: this.state.editName,
+      tags: this.state.editTags,
+      title: this.state.editTitle,
+      upvotes: this.state.editUpvotes
+    };
+    if (window.confirm(`Are you sure you want to submit your changes?`)) {
+      firebase
+        .database()
+        .ref("/users/" + this.state.editKey)
+        .set(edit);
+
+      alert("Changes submitted.");
+    }
+  };
   render() {
     console.log(this.state);
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 20 },
+        sm: { span: 6 }
+      },
+      WrapperCol: {
+        xs: { span: 1 },
+        sm: { span: 1 }
+      }
+    };
+    // edit User Page
+    if (this.state.edit) {
+      return (
+        <div>
+          {" "}
+          <br />
+          <br />
+          <center>
+            <Card
+              title={
+                <div className="panelheader2">
+                  <center>Edit User: {this.state.editUser.name}</center>
+                </div>
+              }
+              style={{ width: 450 }}
+            >
+              <Form>
+                <center>
+                  <Form.Item {...formItemLayout} label="Name">
+                    <div className="input">
+                      <Input
+                        defaultValue={this.state.editName}
+                        onChange={e =>
+                          this.updateEditUser("editName", e.target.value)
+                        }
+                      />{" "}
+                    </div>
+                  </Form.Item>
+                  <Form.Item {...formItemLayout} label="Email">
+                    <div className="input">
+                      <Input
+                        defaultValue={this.state.editEmail}
+                        onChange={e =>
+                          this.updateEditUser("editEmail", e.target.value)
+                        }
+                      />{" "}
+                    </div>
+                  </Form.Item>
+                  <br />
+                  <Form.Item {...formItemLayout} label="About">
+                    <div className="input">
+                      <Input.TextArea
+                        rows={4}
+                        defaultValue={this.state.editAbout}
+                        onChange={e =>
+                          this.updateEditUser("editAbout", e.target.value)
+                        }
+                      />
+                    </div>
+                  </Form.Item>
+                  <br />
+                  <Form.Item {...formItemLayout} label="Upvotes">
+                    <div className="input">
+                      <InputNumber
+                        defaultValue={this.state.editUpvotes}
+                        onChange={value =>
+                          this.updateEditUser("editUpvotes", value)
+                        }
+                      />
+                    </div>
+                  </Form.Item>
+                  <Form.Item {...formItemLayout} label="Title">
+                    <Radio.Group
+                      onChange={e =>
+                        this.updateEditUser("editTitle", e.target.value)
+                      }
+                      defaultValue={this.state.editTitle}
+                    >
+                      <Radio value="Intern">Intern</Radio>
+                      <Radio value="Alumni">Alumni</Radio>
+                      <Radio value="Admin">Admin</Radio>
+                    </Radio.Group>{" "}
+                  </Form.Item>
+                  <Button type="primary" onClick={this.submitChanges}>
+                    Submit Changes
+                  </Button>
+                  {"  "}
+                  <Button onClick={this.cancel}>Cancel</Button>
+                </center>
+              </Form>
+            </Card>
+            <br />
+          </center>
+        </div>
+      );
+    }
+    // returns list of users
     return (
       <div>
         <br />
@@ -133,12 +298,15 @@ export default class User extends Component {
               <center>
                 <Card
                   title={
-                    <div className="panelheader">
+                    <div className="panelheader2">
                       <center>
                         <div className="headertitle">{user.name}</div>
                       </center>
-                      <div className="chaldelete">
-                        <Button size="small">
+                      <div className="editbut">
+                        <Button
+                          size="small"
+                          onClick={e => this.editUnapproved(e, user)}
+                        >
                           <Icon type="edit" />
                         </Button>
                       </div>
@@ -148,8 +316,20 @@ export default class User extends Component {
                 >
                   <p>Email: {user.email} </p>
                   <p>Title: {user.title} </p>
-                  <Button onClick={() => this.acceptUser(user)}>Accept</Button>
-                  <Button onClick={e => this.deleteUser(e, user)}>
+                  <Button
+                    size="small"
+                    type="primary"
+                    onClick={() => this.acceptUser(user)}
+                  >
+                    Accept
+                  </Button>
+                  {"    "}
+                  {"   "}
+                  <Button
+                    size="small"
+                    type="danger"
+                    onClick={e => this.deleteUser(e, user)}
+                  >
                     Reject
                   </Button>
                 </Card>
@@ -163,12 +343,15 @@ export default class User extends Component {
               <center>
                 <Card
                   title={
-                    <div className="panelheader">
+                    <div className="panelheader2">
                       <center>
                         <div className="headertitle">{user.name}</div>
                       </center>
-                      <div className="chaldelete">
-                        <Button size="small">
+                      <div className="editbut">
+                        <Button
+                          onClick={e => this.editUnapproved(e, user)}
+                          size="small"
+                        >
                           <Icon type="edit" />
                         </Button>
                       </div>
@@ -178,8 +361,10 @@ export default class User extends Component {
                 >
                   <p>Email: {user.email} </p>
                   <p>Title: {user.title} </p>
-                  <Button onClick={() => this.acceptUser(user)}>Accept</Button>
-                  <Button onClick={e => this.deleteUser(e, user)}>
+                  <Button type="primary" onClick={() => this.acceptUser(user)}>
+                    Accept
+                  </Button>
+                  <Button type="danger" onClick={e => this.deleteUser(e, user)}>
                     Reject
                   </Button>
                 </Card>
@@ -193,17 +378,18 @@ export default class User extends Component {
               <center>
                 <Card
                   title={
-                    <div className="panelheader">
-                      <center>
-                        <div className="headertitle">
-                          {user.name +
-                            " - " +
-                            user.title.charAt(0).toUpperCase() +
-                            user.title.slice(1)}
-                        </div>
-                      </center>
-                      <div className="chaldelete">
-                        <Button size="small">
+                    <div className="panelheader2">
+                      <div className="headertitle">
+                        {user.name +
+                          " - " +
+                          user.title.charAt(0).toUpperCase() +
+                          user.title.slice(1)}
+                      </div>
+                      <div className="editbut">
+                        <Button
+                          size="small"
+                          onClick={e => this.editUnapproved(e, user)}
+                        >
                           <Icon type="edit" />
                         </Button>
                       </div>
@@ -217,9 +403,12 @@ export default class User extends Component {
                   <p>About: {user.about} </p>
                   <p>Tags: {user.tags}</p>
 
-                  <Button onClick={() => this.acceptUser(user)}>Accept</Button>
-                  <Button onClick={e => this.deleteUser(e, user)}>
-                    Reject
+                  <Button
+                    type="danger"
+                    size="small"
+                    onClick={e => this.deleteUser(e, user)}
+                  >
+                    Remove User
                   </Button>
                 </Card>
                 <br />
@@ -232,7 +421,7 @@ export default class User extends Component {
               <center>
                 <Card
                   title={
-                    <div className="panelheader">
+                    <div className="panelheader2">
                       <center>
                         <div className="headertitle">
                           {user.name +
@@ -241,8 +430,11 @@ export default class User extends Component {
                             user.title.slice(1)}
                         </div>
                       </center>
-                      <div className="chaldelete">
-                        <Button size="small">
+                      <div className="editbut">
+                        <Button
+                          size="small"
+                          onClick={e => this.editUnapproved(e, user)}
+                        >
                           <Icon type="edit" />
                         </Button>
                       </div>
