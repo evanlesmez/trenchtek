77986bba57e347f0af9f7dd44b9d937cbcc3c8b3
Noah French <noahjfrench@gmail.com>
2018-06-20 10:47:03 -0400

    move tabs around on the user nav-bar to make it more intuitive

--- src/TopbarUser.js
+++ src/TopbarUser.js
@@ -29,6 +29,21 @@ class TopbarUser extends Component {
               <div className="topbar-tab">profile</div>
             </Link>
           </Menu.Item>
+          <Menu.Item key="/browse-contracts">
+            <Link to="/browse-contracts">
+              <div className="topbar-tab">contracts</div>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/q&a">
+            <Link to="/q&a">
+              <div className="topbar-tab">q&a</div>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/directory">
+            <Link to="/directory">
+              <div className="topbar-tab">directory</div>
+            </Link>
+          </Menu.Item>
           <Menu.SubMenu
             title={<div className="topbar-tab">interns</div>}
             key="/interns"
@@ -43,30 +58,9 @@ class TopbarUser extends Component {
                 <div className="topbar-tab">resources</div>
               </Link>
             </Menu.Item>
-          </Menu.SubMenu>
-
-          <Menu.Item key="/task-manager">
-            <Link to="/task-manager">
-              <div className="topbar-tab">task manager</div>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/browse-contracts">
-            <Link to="/browse-contracts">
-              <div className="topbar-tab">contracts</div>
-            </Link>
-          </Menu.Item>
-          <Menu.SubMenu
-            title={<div className="topbar-tab">connect</div>}
-            key="/connect"
-          >
-            <Menu.Item key="/q&a">
-              <Link to="/q&a">
-                <div className="topbar-tab">Q&A</div>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="/directory">
-              <Link to="/directory">
-                <div className="topbar-tab">directory</div>
+            <Menu.Item key="/task-manager">
+              <Link to="/task-manager">
+                <div className="topbar-tab">task manager</div>
               </Link>
             </Menu.Item>
           </Menu.SubMenu>
