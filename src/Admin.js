372b53f5d6bdf584a2ae68e5b6a3c814ed8cd8ba
Noah French <noahjfrench@gmail.com>
2018-06-20 14:14:06 -0400

    Manage Contracts is now fully functional, baby!

--- src/Admin.js
+++ src/Admin.js
@@ -1,26 +1,57 @@
 import React, { Component } from "react";
 import firebase from "./Firebase.js";
-import { Collapse, Card, Button } from "antd";
+import { Collapse, Card, Select } from "antd";
 const Panel = Collapse.Panel;
 
 export default class Admin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dataFromDatabase: []
+      dataFromPending: [],
+      dataFromApproved: [],
+      dataFromRejected: [],
+      contractsToView: "pending"
     };
   }
 
+  handleViewChange = value => {
+    this.setState({
+      contractsToView: value
+    });
+  };
+
   componentDidMount() {
-    let database = firebase.database().ref("pendingCompanyContracts");
-    database.on("value", snapshot => {
+    let pendingDatabase = firebase.database().ref("pendingCompanyContracts");
+    pendingDatabase.on("value", snapshot => {
       let newStateFromDB = [];
       snapshot.forEach(function(snap) {
         let obj = { id: snap.key, arrayData: snap.val() };
         newStateFromDB.push(obj);
       });
       this.setState({
-        dataFromDatabase: newStateFromDB
+        dataFromPending: newStateFromDB
+      });
+    });
+    let approvedDatabase = firebase.database().ref("approvedCompanyContracts");
+    approvedDatabase.on("value", snapshot => {
+      let newStateFromDB = [];
+      snapshot.forEach(function(snap) {
+        let obj = { id: snap.key, arrayData: snap.val() };
+        newStateFromDB.push(obj);
+      });
+      this.setState({
+        dataFromApproved: newStateFromDB
+      });
+    });
+    let rejectedDatabase = firebase.database().ref("rejectedCompanyContracts");
+    rejectedDatabase.on("value", snapshot => {
+      let newStateFromDB = [];
+      snapshot.forEach(function(snap) {
+        let obj = { id: snap.key, arrayData: snap.val() };
+        newStateFromDB.push(obj);
+      });
+      this.setState({
+        dataFromRejected: newStateFromDB
       });
     });
   }
@@ -44,24 +75,56 @@ export default class Admin extends Component {
       .ref("pendingCompanyContracts")
       .child(e.target.className)
       .remove();
+    firebase
+      .database()
+      .ref("approvedCompanyContracts")
+      .child(e.target.className)
+      .remove();
   }
 
   //takes the contract from "unapprovedCompanyContracts" from firebase and moves it to
   // approved contracts
   approveContract(e) {
-    console.log(e.target.className);
-    let database = firebase.database();
-    let eventContract = database
-      .ref("pendingCompanyContracts")
-      .child(e.target.className);
-    console.log(eventContract);
-    eventContract.on("value", snapshot => {
-      database.ref("approvedCompanyContracts").push(snapshot.val());
-    });
+    if (this.state.contractsToView === "pending") {
+      let database = firebase.database();
+      let eventContract = database
+        .ref("pendingCompanyContracts")
+        .child(e.target.className);
+      eventContract.on("value", snapshot => {
+        database.ref("approvedCompanyContracts").push(snapshot.val());
+      });
+      database
+        .ref("pendingCompanyContracts")
+        .child(e.target.className)
+        .remove();
+    } else if (this.state.contractsToView === "rejected") {
+      let database = firebase.database();
+      let eventContract = database
+        .ref("rejectedCompanyContracts")
+        .child(e.target.className);
+      eventContract.on("value", snapshot => {
+        database.ref("approvedCompanyContracts").push(snapshot.val());
+      });
+      database
+        .ref("rejectedCompanyContracts")
+        .child(e.target.className)
+        .remove();
+    }
   }
 
   render() {
-    let stateArray = this.state.dataFromDatabase;
+    let stateArray;
+    let titleWords;
+    if (this.state.contractsToView === "pending") {
+      stateArray = this.state.dataFromPending;
+      titleWords = "Manage Pending Contracts";
+    } else if (this.state.contractsToView === "approved") {
+      stateArray = this.state.dataFromApproved;
+      titleWords = "Manage Approved Contracts";
+    } else if (this.state.contractsToView === "rejected") {
+      stateArray = this.state.dataFromRejected;
+      titleWords = "Manage Rejected Contracts";
+    }
     let display = stateArray.map(item => {
       return (
         <div>
@@ -79,24 +142,31 @@ export default class Admin extends Component {
               {item.arrayData.companyEmail}
               <br />
               <br />
-              <center>
-                <button
-                  className={item.id}
-                  onClick={e => this.approveContract(e)}
-                  type="primary"
-                >
-                  Approve Contract
-                </button>
-                <button
-                  className={item.id}
-                  onClick={e => this.rejectContract(e)}
-                  type="danger"
-                >
-                  Reject Contract
-                </button>
-              </center>
+              {this.state.contractsToView !== "approved" && (
+                <center>
+                  <button
+                    className={item.id}
+                    onClick={e => this.approveContract(e)}
+                    type="primary"
+                  >
+                    Approve Contract
+                  </button>
+                </center>
+              )}
+              {this.state.contractsToView !== "rejected" && (
+                <center>
+                  <button
+                    className={item.id}
+                    onClick={e => this.rejectContract(e)}
+                    type="danger"
+                  >
+                    Reject Contract
+                  </button>
+                </center>
+              )}
             </Panel>
           </Collapse>
+          <br />
         </div>
       );
     });
@@ -104,8 +174,22 @@ export default class Admin extends Component {
     return (
       <div>
         <br />
+        <center>
+          <div style={{ display: "inline" }}>Display: </div>
+          <Select
+            defaultValue="pending"
+            onChange={value => {
+              this.handleViewChange(value);
+            }}
+          >
+            <Select.Option value="pending">Pending Contracts</Select.Option>
+            <Select.Option value="approved">Approved Contracts</Select.Option>
+            <Select.Option value="rejected">Rejected Contracts</Select.Option>
+          </Select>
+        </center>
+        <br />
         <Card
-          title={<div className="center-text">Manage Contracts</div>}
+          title={<div className="center-text">{titleWords}</div>}
           style={{ width: 720, margin: "auto" }}
         >
           {display}
