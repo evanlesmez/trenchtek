bab1a320a86a6d7438f326a415c0799e01f967c2
eve9du <eve9du@virginia.edu>
2018-06-21 11:45:16 -0400

    added more functionality in the editing of meet.js in admin page

--- src/EditingForm.js
+++ src/EditingForm.js
@@ -4,7 +4,7 @@ const { TextArea } = Input;
 const EditingForm = props => {
 
     return (
-        <TextArea rows={4} defaultValue={props.defaultValue} enterButton={props.enterButton} onPressEnter={props.onPressEnter} style={{ width: "40%", height: "20%" }} />
+        <TextArea rows={4} defaultValue={props.defaultValue} enterButton={props.enterButton} onPressEnter={props.onPressEnter} />
     );
 }
 
